d87d6833fbb31e53509337ebad8e58f4b4392e5c
Siyu-Sun <siyusun1690@gmail.com>
2023-04-20 22:19:12 -0700

    update

--- Siyu_Sun.js
+++ Siyu_Sun.js
@@ -4,18 +4,3 @@ var height = document.documentElement.scrollHeight - document.documentElement.cl
 var scrolled = (winScroll / height) * 100;
 document.getElementById("myBar").style.width = scrolled + "%";
 
-var acc = document.getElementsByClassName("sections");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
-}
-
