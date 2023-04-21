a72705177332c85ce3c30c50022155e87d3d8111
Siyu-Sun <siyusun1690@gmail.com>
2023-04-20 21:54:06 -0700

    new version

--- Siyu_Sun.js
+++ Siyu_Sun.js
@@ -1,6 +1,4 @@
-window.onscroll = function() {myFunction()};
-
-function myFunction() {
+window.onscroll = function() {theFunction()};
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
