66484033ddfdf5f53d3b48e19460550db4cace39
Siyu-Sun <siyusun1690@gmail.com>
2023-04-20 22:01:45 -0700

    new

--- Siyu_Sun.js
+++ Siyu_Sun.js
@@ -1,6 +1,6 @@
-window.onscroll = function() {theFunction()};
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
+window.onscroll = function () { theFunction() };
+var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+var scrolled = (winScroll / height) * 100;
+document.getElementById("myBar").style.width = scrolled + "%";
+
