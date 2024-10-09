a892b5656d626c4befbda328e197c7cae184bfe4
Siyu-Sun <siyus6@uci.edu>
2024-10-08 20:23:48 -0400

    revise on the website content

--- Siyu_Sun.js
+++ Siyu_Sun.js
@@ -2,5 +2,6 @@ window.onscroll = function () { theFunction() };
 var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
 var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
 var scrolled = (winScroll / height) * 100;
-document.getElementById("myBar").style.width = scrolled + "%";
+document.getElementById("myheader").style.width = scrolled + "%";
+
 
